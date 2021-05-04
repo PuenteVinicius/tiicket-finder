157c86ed1e71aaef1f78ed8fa6853cab82dd8efc
PuenteVinicius <vinicius.puente@thoughtworks.com>
2021-05-04 17:36:10 -0300

    - add debounce to text input

--- src/pages/Home/ArtistInput/ArtistInput.tsx
+++ src/pages/Home/ArtistInput/ArtistInput.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import useDebounce from "../../../utils/hooks";
 import { TextInput } from "grommet";
 
+const DEBOUNCE_DELAY = 3000;
+
 const ArtistInput = ({ callback }: any) => {
-  
-  const [artistNameState, setArtistNameState] = useState({ artistName: "" })
+  const [artistNameState, setArtistNameState] = useState("");
+  const debouncedSearchTerm = useDebounce(artistNameState, DEBOUNCE_DELAY);
 
   const artistNameChangeHandler = (event: any) => {
-    const changedArtistName: string = event.target.value
-    setArtistNameState({ artistName: changedArtistName })
-    callback(artistNameState.artistName)
+    const changedArtistName: string = event.target.value;
+    setArtistNameState(changedArtistName);
   };
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      callback(artistNameState);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchTerm]);
 
   return (
     <TextInput
