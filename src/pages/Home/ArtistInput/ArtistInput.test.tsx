e39ed7b5d764700e2bcdaaa3e2067371e73dc7dd
PuenteVinicius <vinicius.puente@thoughtworks.com>
2021-05-04 17:36:31 -0300

    - fix artist input test

--- src/pages/Home/ArtistInput/ArtistInput.test.tsx
+++ src/pages/Home/ArtistInput/ArtistInput.test.tsx
@@ -1,11 +1,18 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import "jest-styled-components";
+import TicketMasterApi from "../../../api/ticket-master/ticket-master";
 import ArtistInput from "./ArtistInput";
+import useDebounce from "../../../utils/hooks";
 
+const setArtist = async (keyword: string) => {
+  return await TicketMasterApi.getArtist(keyword);
+};
+
+const ARTIST_NAME = "Ariana Grande";
 describe("ArtistInput component", () => {
   let artistInput: any;
   beforeEach(() => {
-    artistInput = render(<ArtistInput />);
+    artistInput = render(<ArtistInput callback={setArtist} />);
   });
   it("Should render text correctly", () => {
     expect(
@@ -18,10 +25,14 @@ describe("ArtistInput component", () => {
   });
 
   describe("Should be fired when an astist has changed", () => {
-    it("When user type a band or an artist name", () => {
+    it("When user type a band or an artist name", async () => {
       let input = screen.getByTestId("artist-input");
-      fireEvent.change(input, { target: { value: "Dua Lipa" } });
-      expect(artistInput.getByDisplayValue("Dua Lipa")).toBeInTheDocument();
+
+      fireEvent.change(input, { target: { value: ARTIST_NAME } });
+      expect(artistInput.getByDisplayValue(ARTIST_NAME)).toBeInTheDocument();
+      setTimeout(() => {
+        expect(useDebounce({}, 500)).toBeCalledTimes(1);
+      }, 3000)
     });
   });
 });
