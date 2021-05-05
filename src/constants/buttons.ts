aa1fec8609146206c2f40c273a97b058d6439005
PuenteVinicius <vinicius.puente@thoughtworks.com>
2021-05-05 15:42:03 -0300

    - remove comments from button

--- src/constants/buttons.ts
+++ src/constants/buttons.ts
@@ -1,10 +1,6 @@
-// This follows Grommet's theme structure.
-// For more info on which attributes are available
-
 import { ButtonType } from 'grommet'
 import { COLORS } from './colors'
 
-// visit https://v2.grommet.io/button
 export const BUTTONS: ButtonType = {
   primary: true,  
   color: COLORS.brand,
