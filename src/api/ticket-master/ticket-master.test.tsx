2493a284c18ba3ca8a51083cacf44cb8ed144c1b
PuenteVinicius <vinicius.puente@thoughtworks.com>
2021-05-05 15:41:44 -0300

    add test for errors

--- src/api/ticket-master/ticket-master.test.tsx
+++ src/api/ticket-master/ticket-master.test.tsx
@@ -1,7 +1,7 @@
 import TicketMasterApi from "./ticket-master";
-
 const ARTIST_NAME = "Ariana Grande";
 const ARTIST_ID = "K8vZ9172Qo7";
+
 describe("Artist", () => {
   jest.setTimeout(6000);
   it("Correct artist information must show correct values", async () => {
@@ -36,8 +36,17 @@ describe("Artist", () => {
     expect(classifications[0].type.name).toEqual("Individual");
     expect(classifications[0].subType.name).toEqual("Musician");
   });
-  it('should not return a vallue when artist name is less than 3 caractres', async () => {
-    const attraction = await TicketMasterApi.getArtist('do');
+  it("should not return a vallue when artist name is less than 3 caractres", async () => {
+    const attraction = await TicketMasterApi.getArtist("do");
     expect(attraction).toEqual({});
+  });
+  it("When artist endpoint returns an error", () => {
+    const mockTicketMasterApi = jest.spyOn(TicketMasterApi, "getArtist");
+    const getArtist = () => {
+      return Error;
+    };
+    mockTicketMasterApi.mockReturnValue(getArtist());
+    const attraction = TicketMasterApi.getArtist(ARTIST_NAME);
+    expect(attraction).toEqual(Error);
   });
 });
